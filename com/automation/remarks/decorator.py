bc6e4697392b93979665accfd548fd9aacb02b86
sergey <sergey@gmail.com>
2016-07-05 18:59:02 +0300

    video decorator refactoring

--- com/automation/remarks/decorator.py
+++ com/automation/remarks/decorator.py
@@ -4,10 +4,12 @@ from time import strftime
 
 from com.automation.remarks.recorder.video_recorder import VideoRecorder
 from com.automation.remarks.utils.file_utils import create_dir
+import inspect
 
 
 def video(enabled=True, name=None):
     def video_decorator(func):
+
         def wrapper(*args, **kwargs):
             file_path = get_file_path(name, func)
             recorder = VideoRecorder(file_path)
@@ -21,11 +23,20 @@ def video(enabled=True, name=None):
 
         return wrapper
 
+    def get_file_path(expected_name, func):
+        dir_path = expanduser("~") + os.sep + 'video'  # video folder path
+        create_dir(dir_path)  # create video folder if not exists
+        file_name = '{0}_{1}.mp4'.format(expected_name or func.func_name, strftime("%Y_%m_%d_%H_%M_%S"))  # format timestamp
+        return dir_path + os.sep + file_name  # save file to user_home/video directory
+
     return video_decorator
 
 
-def get_file_path(name, func):
-    dir_path = expanduser("~") + os.sep + 'video'  # video folder path
-    create_dir(dir_path)  # create video folder if not exists
-    file_name = '{0}_{1}.mp4'.format(name or func.func_name, strftime("%Y_%m_%d_%H_%M_%S"))
-    return dir_path + os.sep + file_name
+def class_recorder(decorator, prefix='test_'):
+    def wrapper(cls):
+        for name, m in inspect.getmembers(cls, inspect.ismethod):
+            if name.startswith(prefix):
+                setattr(cls, name, decorator(m))
+        return cls
+
+    return wrapper
