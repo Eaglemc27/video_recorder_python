20e13d29b799100bf09260783ee6eec0cdb2a6f3
sergey <sergey@gmail.com>
2016-07-05 16:23:38 +0300

    Deletion of succesfull video files added

--- com/automation/remarks/decorator.py
+++ com/automation/remarks/decorator.py
@@ -1,19 +1,31 @@
+import os
+from os.path import expanduser
 from time import strftime
 
 from com.automation.remarks.recorder.video_recorder import VideoRecorder
+from com.automation.remarks.utils.file_utils import create_dir
 
 
 def video(enabled=True, name=None):
     def video_decorator(func):
         def wrapper(*args, **kwargs):
-            recorder = VideoRecorder('{0}_{1}.mp4'.format(name or func.func_name, strftime("%Y_%m_%d_%H_%M_%S")))
+            file_path = get_file_path(name, func)
+            recorder = VideoRecorder(file_path)
             recorder.video_enabled = enabled
             recorder.start_recording()
             try:
                 func(*args, **kwargs)
+                os.remove(file_path)
             finally:
                 recorder.stop_recording()
 
         return wrapper
 
     return video_decorator
+
+
+def get_file_path(name, func):
+    dir_path = expanduser("~") + os.sep + 'video'  # video folder path
+    create_dir(dir_path)  # create video folder if not exists
+    file_name = '{0}_{1}.mp4'.format(name or func.func_name, strftime("%Y_%m_%d_%H_%M_%S"))
+    return dir_path + os.sep + file_name
--- com/automation/remarks/recorder/video_recorder.py
+++ com/automation/remarks/recorder/video_recorder.py
@@ -1,12 +1,8 @@
 import logging
-import os
 import signal
 import subprocess as sp
 
 import tkinter as tk
-
-from com.automation.remarks.utils.file_utils import create_dir
-from os.path import expanduser
 
 
 class VideoRecorder:
@@ -16,9 +12,6 @@ class VideoRecorder:
         self.video_enabled = True
 
     def start_recording(self):
-        dir_path = expanduser("~") + os.sep + 'video'  # video folder path
-        create_dir(dir_path)  # create video folder if not exists
-
         command = ['ffmpeg',
                    '-y',  # (optional) overwrite output file if it exists
                    '-f', 'x11grab',  # grab video from source
@@ -26,7 +19,7 @@ class VideoRecorder:
                    '-r', '24',  # frames per second
                    '-i', ':0.0',  # The imput comes from a pipe
                    '-an',  # Tells FFMPEG not to expect any audio
-                   dir_path + os.sep + self.filename]
+                   self.filename]
 
         if self.video_enabled:
             self.proc = sp.Popen(command, stdin=sp.PIPE, stderr=sp.PIPE)
