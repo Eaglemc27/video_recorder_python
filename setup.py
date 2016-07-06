3295dac58cd024e9d363715fe606f74482d22038
Sergey Pirogov <sepi@administrators-MacBook-Pro.local>
2016-07-06 14:14:25 +0300

    Files added to publish package to Pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'video_recorder',
+  packages = ['video_recorder'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A library that help automation QA engineers to solve problems with flaky tests by recording video.',
+  author = 'Sergey Pirogov',
+  author_email = 'semen4ik20@gmail.com',
+  url = 'https://github.com/SergeyPirogov/video_recorder_python', # use the URL to the github repo
+  download_url = 'https://github.com/SergeyPirogov/video_recorder_python/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'video','test automation'], # arbitrary keywords
+  classifiers = [],
+)
